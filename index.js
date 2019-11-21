bbf16c5dbf98a380d3fbdcc3f041070cdd3294fc
JaecholJeon <wjswocjf92@gmail.com>
2019-11-22 06:46:39 +0900

    Add function mysql

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 const express = require("express"),
   http = require("http"),
   cors = require("cors"),
+  mysql = require("mysql"),
+  fs = require("fs"),
+  SqlToJson = require("sql-to-json"),
+  readline = require("readline"),
+  request = require("request"),
   bodyParser = require("body-parser");
+var host, db, user, passward;
 const app = express();
-const port = 8080;
+const port = 10101;
 var router = express.Router();
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  passward: "wjswocjf20",
+  database: "dbown",
+  insecureAuth: true
+});
+connection.connect();
+connection.query("SELECT * FROM grp_prdt_gaip_info", function(
+  error,
+  results,
+  fields
+) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
 app.use(cors());
 app.set("port", process.env.PORT || port);
 app.use(bodyParser.json());
 app.all("/", function(req, res) {
-  res.send("shinago Project");
   console.log("home");
 });
+
 router.route("/cardlist").post((req, res) => {
-  res.send("POST Card Product Lists");
   console.log(req.body);
 });
+
 router.route("/user").get((req, res) => {});
+router.route("/");
 app.use(router);
+
 http.createServer(app).listen(app.get("port"), () => {
   console.log(`Api  Server Start on ` + port);
 });
